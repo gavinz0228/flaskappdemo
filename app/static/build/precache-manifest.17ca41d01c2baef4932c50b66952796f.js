8daafc6c28e47a35e8adec3bd05ce3c73a0a0904
gavin <gavinz0228@gmail.com>
2019-09-09 09:57:53 -0400

    check in build folder

---
+++ app/static/build/precache-manifest.17ca41d01c2baef4932c50b66952796f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0128394deb9ef201e954cdf73ef3fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9931b6d9695208167b16",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1abf10bcc5e7d3bffe89",
+    "url": "/static/js/2.13deb92f.chunk.js"
+  },
+  {
+    "revision": "9931b6d9695208167b16",
+    "url": "/static/js/main.a764bec4.chunk.js"
+  },
+  {
+    "revision": "46ec364e5da4f306bbc3",
+    "url": "/static/js/runtime~main.9a25287a.js"
+  }
+]);
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import sys
 from app.api import create_app
+
+port = int(sys.argv[1])
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=port)
